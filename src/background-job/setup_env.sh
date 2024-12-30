d1f3f303fd9eb9c0b11a05263ec5b5d87d8a125c
Iwan Firmawan <firmawaneiwan@gmail.com>
2024-12-30 10:44:19 +0700

    chore: remove unused setup_env.sh

--- src/background-job/setup_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the root directory of the project
-root_path="$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")"
-
-# Load environment variables from .env file in the root directory
-export $(grep -v '^#' "$root_path/.env" | xargs)
