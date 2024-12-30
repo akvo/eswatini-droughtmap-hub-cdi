5b9015d583045dd39f21a2937e9cb2b2fb9930e7
Iwan Firmawan <firmawaneiwan@gmail.com>
2024-12-30 13:56:28 +0700

    [#1] Simplify login_and_download script

--- src/background-job/job_00_login.sh
+++ src/background-job/job_00_login.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 login_and_download_cookies() {
-    # Remove .urs_cookies file and MERRA2_100.tavgM_2d_slv_Nx.198101.nc4 file if they exist
-    if [ -f ./.urs_cookies ]; then
-        rm ./.urs_cookies
-    fi
-    if [ -f MERRA2_100.tavgM_2d_slv_Nx.198101.nc4 ]; then
-        rm MERRA2_100.tavgM_2d_slv_Nx.198101.nc4
+    # Remove .urs_cookies and MERRA2_100.tavgM_2d_slv_Nx.198101.nc4 files if they exist and are older than 1 day
+    find . -name ".urs_cookies" -o -name "MERRA2_100.tavgM_2d_slv_Nx.198101.nc4" -mtime +1 -exec rm {} \;
+
+    # Check if .urs_cookies and MERRA2_100.tavgM_2d_slv_Nx.198101.nc4 exist and are created today
+    if [ -f ./.urs_cookies ] && [ -f MERRA2_100.tavgM_2d_slv_Nx.198101.nc4 ] && [ $(find ./.urs_cookies -mtime -1) ] && [ $(find MERRA2_100.tavgM_2d_slv_Nx.198101.nc4 -mtime -1) ]; then
+        echo "Files are up to date. Skipping login and download."
+        return 0
     fi
 
     wget --load-cookies ./.urs_cookies \
@@ -17,7 +18,6 @@ login_and_download_cookies() {
         --content-disposition \
         https://goldsmr4.gesdisc.eosdis.nasa.gov/data/MERRA2_MONTHLY/M2TMNXSLV.5.12.4/1981/MERRA2_100.tavgM_2d_slv_Nx.198101.nc4 > /dev/null 2>&1
 
-    # Check if the download was successful
     if [ $? -ne 0 ]; then
         echo "Login failed"
         exit 1
