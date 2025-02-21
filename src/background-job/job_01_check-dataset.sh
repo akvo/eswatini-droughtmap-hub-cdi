c05f642ed80d29c470306e17bb44bf3e81ed8884
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-02-21 23:46:51 +0700

    [#7] Add more clear messages in job_01

--- src/background-job/job_01_check-dataset.sh
+++ src/background-job/job_01_check-dataset.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 check_and_download_chirps_dataset() {
+    echo "Check and download CHIRPS dataset"
     remove_tmp_files "../../input_data/CHIRPS"
 
     # Check if all-CHIRPS.log exists
@@ -8,16 +9,27 @@ check_and_download_chirps_dataset() {
         touch "../../logs/all-CHIRPS.log"
     fi
 
-    curl -s "${DOWNLOAD_CHIRPS_BASE_URL}" \
-        | grep "${DOWNLOAD_CHIRPS_PATTERN}" \
-        | pup \
-        | grep -v "href" \
-        | grep "${DOWNLOAD_CHIRPS_PATTERN}" \
-        | sed "s/\ //g" > "../../logs/all-CHIRPS.log"
+    LOG_IS_EMPTY=$(cat "../../logs/all-CHIRPS.log" | wc -l)
+
+    # If the log file is empty or the last line is 2 previous months
+    # based on this pattern chirps-v2.0.{year}.{month}.tif.gz
+    # then update the log file
+    if [ "$LOG_IS_EMPTY" -eq 0 ] || [ "$(tail -n 1 "../../logs/all-CHIRPS.log" | cut -d '.' -f 3)" -lt "$(date -d "2 months ago" "+%Y")" ]; then
+        echo "Update all-CHIRPS.log"
+        curl -s "${DOWNLOAD_CHIRPS_BASE_URL}" \
+            | grep "${DOWNLOAD_CHIRPS_PATTERN}" \
+            | pup \
+            | grep -v "href" \
+            | grep "${DOWNLOAD_CHIRPS_PATTERN}" \
+            | sed "s/\ //g" > "../../logs/all-CHIRPS.log"
+    fi
 
     if ls ../../input_data/CHIRPS/*.tif* 1> /dev/null 2>&1; then
         num_files_in_dir=$(get_num_files_in_dir "../../input_data/CHIRPS")
         num_files_in_log=$(get_num_files_in_log "../../logs/all-CHIRPS.log")
+
+        echo "Number of files in CHIRPS directory: ${num_files_in_dir}"
+        echo "Number of files in all-CHIRPS.log log file: ${num_files_in_log}"
 
         if [ "$num_files_in_dir" -lt "$num_files_in_log" ]; then
             download_missing_files "../../input_data/CHIRPS" "../../logs/all-CHIRPS.log" "${DOWNLOAD_CHIRPS_BASE_URL}" ".tif*"
@@ -31,13 +43,16 @@ check_and_download_chirps_dataset() {
 }
 
 check_and_download_LST_dataset() {
+    echo "Check and download LST dataset"
     check_and_create_download_log  "${DOWNLOAD_LST_BASE_URL}" "${DOWNLOAD_LST_PATTERN}" "LST"
 }
 
 check_and_download_NDVI_dataset() {
+    echo "Check and download NDVI dataset"
     check_and_create_download_log  "${DOWNLOAD_NDVI_BASE_URL}" "${DOWNLOAD_NDVI_PATTERN}" "NDVI"
 }
 
 check_and_download_SM_dataset() {
+    echo "Check and download SM dataset"
     check_and_create_download_log  "${DOWNLOAD_SM_BASE_URL}" "${DOWNLOAD_SM_PATTERN}" "SM"
 }
