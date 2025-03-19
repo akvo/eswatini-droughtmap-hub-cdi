344fa5a4ece9eb490a268b2417f98dd8b75c2aa4
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-03-19 14:42:12 +0700

    [#9] Remove commented-out code for executing CDI scripts

--- src/background-job/job_03_run_cdi.sh
+++ src/background-job/job_03_run_cdi.sh
@@ -1,5 +1,6 @@
 run_cdi_scripts() {
     source ~/.myenv/bin/activate
+    # python -u ../data-processing/cdi-scripts/STEP_0000_execute_all_steps.py --mode=all
     python -u ../data-processing/cdi-scripts/STEP_0000_execute_all_steps.py
     if [[ $? -ne 0 ]]; then
         echo "CDI script execution failed!"
