78b1b1b6fd4feb8c77bf6e5ecdf42a360383adf9
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-03-19 14:42:32 +0700

    [#9] Add function to upload CDI to GeoNode with error handling

--- src/background-job/job_04_upload_to_geonode.sh
+++ src/background-job/job_04_upload_to_geonode.sh
@@ -7,3 +7,14 @@ upload_to_geonode () {
     fi
     deactivate
 }
+
+upload_cdi_to_geonode () {
+    source ~/.myenv/bin/activate
+    # Run the script without changing directories
+    python -u upload_to_geonode/upload_cdi_to_geonode_job.py
+    if [[ $? -ne 0 ]]; then
+        echo "Upload CDI to Geonode script execution failed!"
+        exit 1
+    fi
+    deactivate
+}
